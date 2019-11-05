7ed0f83804038c7674c7a6631dc28acc17626687
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-11-05 13:47:13 +0000

    Add concourse user, password and url interpolation

--- namespaces/concourse-op.cloud-platform.service.justice.gov.uk/helloworld-dev/resources/gitops.tf
+++ namespaces/concourse-op.cloud-platform.service.justice.gov.uk/helloworld-dev/resources/gitops.tf
@@ -2,4 +2,7 @@ module "concourse-gitops" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-gitops"
   github_team                   = "webops"
   namespace                     = "helloworld-dev"
+  concourse_basic_auth_username = "${concourse_basic_auth_username}"
+  concourse_url                 = "${concourse_url}"
+  concourse_basic_auth_password = "${concourse_basic_auth_password}"
 }
