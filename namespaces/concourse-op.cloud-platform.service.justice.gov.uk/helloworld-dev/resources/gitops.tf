241996842697d1c97272a87064ba9aa7aa1ebbf9
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-11-06 15:00:08 +0000

    Add new variables required by the gitops module

--- namespaces/concourse-op.cloud-platform.service.justice.gov.uk/helloworld-dev/resources/gitops.tf
+++ namespaces/concourse-op.cloud-platform.service.justice.gov.uk/helloworld-dev/resources/gitops.tf
@@ -1,7 +1,8 @@
 module "concourse-gitops" {
-  source                        = "github.com/ministryofjustice/cloud-platform-terraform-gitops"
+  source_code_url               = "github.com/ministryofjustice/cloud-platform-terraform-gitops"
   github_team                   = "webops"
   namespace                     = "helloworld-dev"
+  branch                        = "add/gitops"
   concourse_basic_auth_username = "${var.concourse_basic_auth_username}"
   concourse_url                 = "${var.concourse_url}"
   concourse_basic_auth_password = "${var.concourse_basic_auth_password}"
