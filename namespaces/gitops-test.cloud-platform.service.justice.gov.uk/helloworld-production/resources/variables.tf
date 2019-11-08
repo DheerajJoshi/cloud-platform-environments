cf5ec7032aee5c05505a118959e03f37a1bd08bd
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-11-08 12:37:46 +0000

    Add helloworld production env

---
+++ namespaces/gitops-test.cloud-platform.service.justice.gov.uk/helloworld-production/resources/variables.tf
@@ -0,0 +1,7 @@
+# These varaibles will be defined by the 
+# cloud-platform-environments pipeline
+variable concourse_basic_auth_username {}
+
+variable concourse_url {}
+
+variable concourse_basic_auth_password {}
