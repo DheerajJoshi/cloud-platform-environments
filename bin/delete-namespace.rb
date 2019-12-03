9bdafd1ed5f5057b4bdab10d35afb6537327b9b8
David Salgado <david@digitalronin.com>
2019-12-03 17:04:58 +0000

    Attempt to delete a real namespace

--- bin/delete-namespace.rb
+++ bin/delete-namespace.rb
@@ -4,4 +4,4 @@ require File.join(".", File.dirname(__FILE__), "..", "lib", "cp_env")
 
 require "pry-byebug"
 
-CpEnv::NamespaceDeleter.new(namespace: "mynamespace").delete
+CpEnv::NamespaceDeleter.new(namespace: "dstest-deva").delete
