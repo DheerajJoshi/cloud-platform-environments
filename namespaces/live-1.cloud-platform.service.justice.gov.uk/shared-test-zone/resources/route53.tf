67e957094b5a928477fda03c6375adce0b38b34d
mtrbls <mourad.trabelsi@digital.justice.gov.uk>
2020-07-15 09:17:11 +0100

    add new GH action to reject multi-namespace PR

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/shared-test-zone/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/shared-test-zone/resources/route53.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_zone" "shared" {
+resource "aws_route53_zone" "test" {
   name = "test.cloud-platform.service.justice.gov.uk"
 
   tags = {
@@ -11,9 +11,11 @@ resource "aws_route53_zone" "shared" {
   }
 }
 
-resource "kubernetes_secret" "example_route53_zone_sec" {
+
+
+resource "kubernetes_secret" "test" {
   metadata {
-    name      = "route53"
+    name      = "route53-test"
     namespace = "shared-test-zone"
   }
 
@@ -21,3 +23,32 @@ resource "kubernetes_secret" "example_route53_zone_sec" {
     zone_id = aws_route53_zone.shared.zone_id
   }
 }
+
+##
+
+resource "aws_route53_zone" "shared" {
+  name = "shared.service.justice.gov.uk"
+
+  tags = {
+    business-unit          = "webops"
+    application            = "shared-zone"
+    is-production          = "false"
+    environment-name       = "dev"
+    owner                  = "webops"
+    infrastructure-support = "platforms@digital.service.justice.gov.uk"
+  }
+}
+
+
+
+resource "kubernetes_secret" "shared" {
+  metadata {
+    name      = "route53-shared"
+    namespace = "shared-test-zone"
+  }
+
+  data = {
+    zone_id = aws_route53_zone.shared.zone_id
+  }
+}
+
